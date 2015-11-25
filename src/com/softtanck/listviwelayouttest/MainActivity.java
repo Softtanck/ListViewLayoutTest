20a9034ba10083aa6654a9d9a63aceb0da09b2c7
Tanck <softtanck@163.com>
2015-11-25 16:08:21 +0800

    add new style

--- src/com/softtanck/listviwelayouttest/MainActivity.java
+++ src/com/softtanck/listviwelayouttest/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends Activity {
 
 		list = new ArrayList<String>();
 
-		for (int i = 0; i < 10; i++) {
+		for (int i = 0; i < 100; i++) {
 			list.add("");
 		}
 		adapter = new Myadapter(MainActivity.this, list);
